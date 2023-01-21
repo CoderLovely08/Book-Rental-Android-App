d6232662008be2bc1d14d36dabe90884280b47c3
Lovely Sharma <lovelysingeras297@gmail.com>
2023-01-21 09:29:05 +0530

    Final commit (most probably)

--- app/src/main/java/com/example/bookrental/LoginActivity.java
+++ app/src/main/java/com/example/bookrental/LoginActivity.java
@@ -20,7 +20,6 @@ import java.io.InputStreamReader;
 public class LoginActivity extends AppCompatActivity {
     TextInputEditText mEmailFieldLogin, mPasswordFieldLogin;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,49 +67,5 @@ public class LoginActivity extends AppCompatActivity {
             editor.apply();
             redirectHome();
         }else Toast.makeText(this, "Incorrect credentials!", Toast.LENGTH_SHORT).show();
-
-//        --------------------------------------------------------------------
-        /*
-        File file = new File(getFilesDir(), "signup_info.txt");
-        if (!file.exists()) {
-            Toast.makeText(this, "Signup information not found", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
-            String line;
-            String name = null;
-            String userEmail =null;
-            while ((line = br.readLine()) != null) {
-
-                if(line.startsWith("Name: ")) {
-                    name = line.substring("Name: ".length());
-                    System.out.println(name);
-                }if (line.startsWith("Email: ") && line.substring(7).equals(email)) {
-                    userEmail=line.substring(7);
-                    while ((line = br.readLine()) != null) {
-                        if (line.startsWith("Password: ") && line.substring(10).equals(password)) {
-                            Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show();
-                            // Save login information in SharedPreferences
-                            SharedPreferences.Editor editor = getSharedPreferences("loginPreference", Context.MODE_PRIVATE).edit();
-                            editor.putBoolean("is_logged_in", true);
-                            System.out.println(name+" "+userEmail);
-                            editor.putString("username",name);
-                            editor.putString("userEmail",userEmail);
-                            editor.apply();
-                            redirectHome();
-                            return;
-                        }
-                    }
-                    Toast.makeText(this, "Incorrect password", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-            }
-            Toast.makeText(this, "Email not found", Toast.LENGTH_SHORT).show();
-        } catch (IOException e) {
-            Toast.makeText(this, "Error reading signup info", Toast.LENGTH_SHORT).show();
-        }
-
-         */
     }
 }
